d49fcf9a130ade1c7ace2ed71f3f840ee8f1f322
birju0451 <birjukumar1252005@gmail.com>
2026-01-26 19:23:06 +0530

    intial comment

--- 1.Basics/1.4VariableNaming rule.py
+++ 1.Basics/1.4VariableNaming rule.py
@@ -1,32 +1,104 @@
-#string
-name="birju"
-print(type(name))
+"""
+File Name: python_datatypes_and_print_notes.py
 
-#integer
-roll_number=216
-print(type(roll_number))
+Topic:
+- Basic Python Data Types
+- print() function
+- Variable update
+- Separator (sep)
+"""
 
-#floating number
-percentage=82.5
-print(type(percentage))
-#boolean
-is_student=True
-print(type(is_student))
+# -------------------------------
+# STRING (str)
+# -------------------------------
+# A string is a sequence of characters written inside quotes.
 
-print(name,roll_number,percentage,is_student )
-print("my name is "+name + " and roll number is ", roll_number,)
-print("I score ",percentage,("%  is my final exam.i am a student"))
-#update
-percentage=94.5
+name = "birju"
+print(type(name))      # <class 'str'>
 
-print(name,percentage)
 
-#print expression
-print("my percentge has change to",percentage + 1)
+# -------------------------------
+# INTEGER (int)
+# -------------------------------
+# Integer represents whole numbers (no decimal point).
 
-#print wih seperAtor
-print(name, roll_number,percentage,is_student,sep="-" )
-x=1
-y=2 
-z=3
-print(x,y,z,sep="->")
+roll_number = 216
+print(type(roll_number))   # <class 'int'>
+
+
+# -------------------------------
+# FLOAT (float)
+# -------------------------------
+# Float represents decimal numbers.
+
+percentage = 82.5
+print(type(percentage))    # <class 'float'>
+
+
+# -------------------------------
+# BOOLEAN (bool)
+# -------------------------------
+# Boolean has only two values: True or False.
+
+is_student = True
+print(type(is_student))    # <class 'bool'>
+
+
+# -------------------------------
+# PRINTING MULTIPLE VALUES
+# -------------------------------
+# print() can display multiple values separated by spaces.
+
+print(name, roll_number, percentage, is_student)
+
+
+# -------------------------------
+# STRING CONCATENATION
+# -------------------------------
+# Strings can be joined using + operator.
+# Numbers should be printed using comma (,).
+
+print("My name is " + name + " and roll number is", roll_number)
+
+
+# -------------------------------
+# PRINTING TEXT WITH NUMBERS
+# -------------------------------
+# Comma allows mixing strings and numbers safely.
+
+print("I scored", percentage, "% in my final exam. I am a student.")
+
+
+# -------------------------------
+# UPDATING A VARIABLE
+# -------------------------------
+# Variable values can be changed during program execution.
+
+percentage = 94.5
+print(name, percentage)
+
+
+# -------------------------------
+# PRINTING EXPRESSIONS
+# -------------------------------
+# Python evaluates expressions inside print().
+
+print("My percentage has changed to", percentage + 1)
+
+
+# -------------------------------
+# USING SEPARATOR (sep)
+# -------------------------------
+# sep changes the default space between printed values.
+
+print(name, roll_number, percentage, is_student, sep="-")
+
+
+# -------------------------------
+# CUSTOM SEPARATOR EXAMPLE
+# -------------------------------
+
+x = 1
+y = 2
+z = 3
+print(x, y, z, sep="->")
